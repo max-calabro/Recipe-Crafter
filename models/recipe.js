d20526011af1e47ad9748d0855143aec23c1c4ca
Max Calabro <maxccalabro@gmail.com>
2022-10-20 12:08:41 -0400

    Recipe model created

---
+++ models/recipe.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Recipe = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    ingredients: { type: Array, required: true },
+    instructions: { type: String, required: true },
+    notes: { type: String, required: true }
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Recipe', Recipe)
