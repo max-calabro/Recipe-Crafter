03102bd7541554704b65a55104ab5110bc6cfe9a
Max Calabro <maxccalabro@gmail.com>
2022-10-20 12:01:01 -0400

    db index is set

---
+++ db/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+let MONGO_URI = 'mongo://127.0.0.1:27017/recipesDatabase'
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Successfully connected to MongoDB.')
+  })
+  .catch((e) => {
+    console.log('Connection error', e.message)
+  })
+
+const db = mongoose.connection
+
+module.exports = db
