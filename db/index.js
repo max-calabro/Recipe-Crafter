af88de62148e5484f7d18003ff477ca97845b353
Max Calabro <maxccalabro@gmail.com>
2022-10-28 10:33:46 -0400

    another try

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 let MONGO_URI =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipesDatabase'
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/recipesDatabase'
+//process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipesDatabase'
 
 mongoose
   .connect(MONGO_URI)
